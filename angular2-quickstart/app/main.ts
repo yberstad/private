7f970adf5aaad8a2f9b80a3f6b317d05762c4a44
Oyvind Habberstad <oyvindhabberstad@gmail.com>
2016-03-11 00:21:39 +0100

    Removed test project

--- angular2-quickstart/app/main.ts
+++
@@ -1,5 +0,0 @@
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-import {SocketService, RestService} from "./../services/feathers.service.ts";
-
-bootstrap(AppComponent, [SocketService, RestService]);
